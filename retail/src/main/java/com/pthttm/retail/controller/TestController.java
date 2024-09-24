269a998165d8421ad0cfc5b7b3ea1ee76e9b0afb
NT-Van-Khanh <ntvk137@gmail.com>
2024-09-24 12:29:53 +0700

    build:frontend of login page

--- retail/src/main/java/com/pthttm/retail/controller/TestController.java
+++ retail/src/main/java/com/pthttm/retail/controller/TestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -12,24 +13,19 @@ import java.util.Base64;
 
 @Controller
 public class TestController {
-    private final FirebaseService firebaseService;
 
-    @Autowired
-    public TestController(FirebaseService firebaseService) {
-        this.firebaseService = firebaseService;
+
+    @GetMapping("/header")
+    public String getHeader(Model model) throws IOException {
+        return "comp-header";
+    }
+    @GetMapping("/")
+    public String getIndex(Model model) throws IOException {
+        return "index";
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/home")
     public String FirstTest (Model model) throws IOException {
-        InputStream imageStream = firebaseService.getObject("logo-web.png");
-        if(imageStream !=null){
-            byte[] imageBytes = imageStream.readAllBytes();
-            String base64Image = Base64.getEncoder().encodeToString(imageBytes);
-            model.addAttribute("base64Image",base64Image);
-        }else{
-            model.addAttribute("base64Image","");
-        }
-
         return "index";
     }
 

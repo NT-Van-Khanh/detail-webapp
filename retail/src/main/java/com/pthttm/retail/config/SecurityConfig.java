4eba81fedcfff1debc6c587d453a3822bb68f185
NT-Van-Khanh <ntvk137@gmail.com>
2024-09-24 12:29:53 +0700

    build:frontend of login page

---
+++ retail/src/main/java/com/pthttm/retail/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.pthttm.retail.config;
+
+import com.google.api.Http;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((requests)->requests
+                    .requestMatchers("/","/home","/css/**").permitAll()
+                    .anyRequest().authenticated())
+                .formLogin((form)->form
+                    .loginPage("/login").permitAll())
+                .logout(LogoutConfigurer::permitAll);
+        return httpSecurity.build();
+    }
+
+}
---
+++ retail/src/main/java/com/pthttm/retail/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.pthttm.retail.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLogin(Model model){
+        return "login";
+    }
+}

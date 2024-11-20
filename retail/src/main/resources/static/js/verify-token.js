cabe8483a829e2aa8d2e89aeb94b91574cef1ca5
NT-Van-Khanh <128962835+NT-Van-Khanh@users.noreply.github.com>
2024-11-20 15:17:55 +0700

    Update verify-token.js

--- retail/src/main/resources/static/js/verify-token.js
+++ retail/src/main/resources/static/js/verify-token.js
@@ -2,13 +2,6 @@
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber  } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
 const firebaseConfig = {
-    apiKey: "AIzaSyAqmCdYn5b7UCtBrU9IX2eNmr-2D3Izc7k",
-    authDomain: "retailstorage-5432c.firebaseapp.com",
-    projectId: "retailstorage-5432c",
-    storageBucket: "retailstorage-5432c.appspot.com",
-    messagingSenderId: "232349606507",
-    appId: "1:232349606507:web:d58ded510636de342b472e",
-    measurementId: "G-2YGTRXPYEH"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);

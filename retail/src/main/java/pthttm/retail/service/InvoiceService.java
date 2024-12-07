b066a217cc884ef8af8d2be5dd635f166e1ace97
TungNguyen2210 <nguyenthanhtung221003@gmail.com>
2024-12-07 17:36:49 +0700

    refactor: delete redundant code

--- retail/src/main/java/pthttm/retail/service/InvoiceService.java
+++ retail/src/main/java/pthttm/retail/service/InvoiceService.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pthttm.retail.model.Invoice;
 import pthttm.retail.repository.InvoiceRepository;
-@Service
-public class InvoiceService {
-	 @Autowired
-	    private InvoiceRepository invoiceRepository;
 
-	    public Invoice saveInvoice(Invoice invoice) {
-	        return invoiceRepository.save(invoice);
-	    }
+public class InvoiceService {
+
+//	    private InvoiceRepository invoiceRepository;
+//
+//	    public Invoice saveInvoice(Invoice invoice) {
+//	        return invoiceRepository.save(invoice);
+//	    }
 }
